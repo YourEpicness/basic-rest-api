1d6c5d064aecfae9641e20af45f5c14801f97950
Julaam Diop <jeebe.diop@gmail.com>
2021-10-19 04:07:28 -0500

    Fix profile CRUD to properly display profile JSON

--- controllers/profiles.controller.js
+++ controllers/profiles.controller.js
@@ -1,11 +1,17 @@
 // used for handling various OS paths
 const path = require('path');
 
+let timestamp = Date.now();
+let current_dt = new Date(timestamp);
 
 const profiles = [
     {
         id: 1,
-        profile: 'First profile!'
+        date_created: current_dt,
+        profile: {
+            name: "John Doe",
+            about: "This is a dummy person"
+        }
     }
 ]
 // best to write using function() instead of arrow function
@@ -14,29 +20,30 @@ function getProfiles(req, res) {
 }
 
 function postProfile(req, res){
-    const newprofile = {
+    const newProfile = {
         id: profiles.length + 1,
+        date_created: current_dt,
         profile: req.body.profile
     }
 
-    profiles.push(newprofile);
-    res.status(201).json({newprofile})
+    profiles.push(newProfile);
+    res.status(201).json({newProfile})
 }
 
 function updateProfile(req, res) {
     const profileId = Number(req.params.id)
     console.log(`Updating profile at profileId:${profileId}...`)
     const profileIndex = profiles.findIndex(item => item.id === profileId)
-    updatedprofile = {
+    const updatedProfile = {
         id: profileId,
         profile: req.body.profile
     }
     if(profileIndex === -1) {
-        profiles.push(updatedprofile)
+        profiles.push(updatedProfile)
     } else {
-        profiles.splice(profileIndex, 1, updatedprofile)
+        profiles.splice(profileIndex, 1, updatedProfile)
     }
-    res.status(200).json(updatedprofile)
+    res.status(200).json(updatedProfile)
 }
 
 function deleteProfile(req, res) {
